fbebdaf7dbe760ddec07e4eb251ef119df3a8142
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-07-28 01:46:08 -0300

    Improved gui

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
+
 import gtk
 from gtk import glade
 from debug import debug
 
-
 class Mainwindow:
-   def __init__(self):
-      self.xml = glade.XML("gui.glade", None, None)
-      self.xml.signal_connect("on_window1_key_press_event", self.key_press)
-      self.xml.signal_connect("on_window1_key_release_event", self.key_release)
-      self.xml.signal_connect("on_window1_destroy", lambda w: gtk.main_quit())
 
-#      self.spin = self.xml.get_widget("fahr")
-#      self.result = self.xml.get_widget("celsius")
+    def __init__(self):
+        self.wtree = glade.XML("gui.glade")
 
-   def key_press(self, w):
-      fahr = self.spin.get_value_as_int();
-      cent = (fahr - 32) / 1.8
-      texto = "%.2f C" % cent
-      self.result.set_label(texto)
+        self.rigth = self.wtree.get_widget("rigth")
+        self.left = self.wtree.get_widget("left")
 
-   def key_press(self, w):
-      fahr = self.spin.get_value_as_int();
-      cent = (fahr - 32) / 1.8
-      texto = "%.2f C" % cent
-      self.result.set_label(texto)
+        self.wtree.signal_autoconnect({
+            "on_main_key_press_event": self.key_press,
+            "on_main_destroy": self.destroy,
+            "on_left_button_press_event": self.left_clicked,
+            "on_rigth_button_press_event": self.rigth_clicked,
+        })
 
- if __name__ == "__main__":
-   ftoc = FtoC()
-   gtk.main()
+
+    def left_clicked(self, w, info):
+        debug(w, info)
+
+    def rigth_clicked(self, w, info):
+        debug(w, info)
+
+    def key_press(self, w, info):
+        debug(w, info)
+        debug(dir(info))
+        debug(info.string)
+        debug(info.hardware_keycode)
+        debug(info.keyval)
+
+    def destroy(self, w):
+        gtk.main_quit()
 
 
 def main():
+    window = Mainwindow()
+    gtk.main()
 
 if __name__ == "__main__":
     exit(main())
