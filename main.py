b3f9c71b8cbc98f7a3414b1c2ed615b9ec5c3cfd
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-08-31 03:39:20 -0300

    cleaned

--- main.py
+++ main.py
@@ -62,26 +62,10 @@ class Mainwindow:
         return debug("Rematch")
 
 
-def cerf(x):
-    """Complementary error function."""
-    z = abs(x)
-    t = 1 / (1 + z / 2.)
-    r = t * exp(- z ** 2 - 1.26551223 + t * (1.00002368 + t * (.37409196 + t *
-        (.09678418 + t * (-.18628806 + t * (.27886807 + t * (-1.13520398 + t *
-        (1.48851587 + t * (-.82215223 + t * .17087277)))))))))
-
-    if (x >= 0.):
-        return r
-    else:
-        return 2. - r
-
-def cdf(x):
-    return 1. - 0.5 * cerf(x / (2 ** 0.5))
-
-
 def main():
     window = Mainwindow()
     gtk.main()
+
 
 if __name__ == "__main__":
     exit(main())
